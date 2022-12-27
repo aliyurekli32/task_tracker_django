e68933cb10e36110ce38b55b76c0cda3c4d4e6c0
aliyurekli32 <adamjesperblack@gmail.com>
2022-12-27 20:51:14 +0300

    first commit

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Todo(models.Model):
+    PRIORITY = (
+        (1, 'High'),
+        (2,'Medium'),
+        (3,'Low')
+    )
+    task = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    priority = models.SmallIntegerField(choices=PRIORITY, default=3)
+    is_done = models.BooleanField(default=False)
+    update_date = models.DateTimeField()
+    craeted_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.task
